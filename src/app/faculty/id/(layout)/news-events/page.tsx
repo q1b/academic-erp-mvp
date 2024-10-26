2e1bd613364681c1ccef44129015e92428c6f117
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-10-26 12:41:16 +0530

    removed deprecated files

--- src/app/faculty/id/(layout)/news-events/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function NewsEvents() {
-    return <div className="flex items-start gap-x-8 mt-20">
-    <div>10th October, 2024</div>
-    <div className="">
-        <h3 className="text-lg/6">Incedo Inc Campus Placement Drive</h3>
-        <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, commodi.</p>
-    </div>
-</div>
-}
